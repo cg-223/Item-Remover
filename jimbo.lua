54624bcf10b8d2df52bb3d39299b49fb39856f01
art-muncher <doomanddesire@gmail.com>
2024-11-14 07:14:25 +0100

    Enabling and disabling + Blind removing

--- jimbo.lua
+++ jimbo.lua
@@ -27,18 +27,57 @@ mod.config["Disabled Things"] = mod.config["Disabled Things"] or {}
 local oldfunc = Card.click
 function Card:click()
     local ret = oldfunc(self)
-    if G.your_collection and G.your_collection[1] and G.your_collection[1].cards then
+    --print(self.config and self.config.center and self.config.center.key and self.config.center.key)
+    --print(self.config and self.config.blind and self.config.blind)
+    
+    local antiVal = nil
+    if self.config.card then antiVal = self.config.center.key end
+    if self.config.blind then antiVal = self.config.blind.key end
+    if self.edition then antiVal = self.edition.key end
+    local dontDisable = {
+        bl_small = true,
+        bl_big = true,
+        j_joker = true,
+        tag_handy = true,
+        v_blank = true,
+        c_incantation = true,
+        c_pluto = true,
+        c_strength = true,
+        c_base,
+        e_base,
+    }
+    if G.your_collection and G.your_collection[1] and G.your_collection[1].cards and self.config and antiVal and not dontDisable[antiVal] then
         for j = 1, #G.your_collection do
             for i = #G.your_collection[j].cards,1, -1 do
             if G.your_collection[j].cards[i] == self then
                 self.debuff = not self.debuff
-                mod.config["Disabled Things"][self.config.center.key] = not mod.config["Disabled Things"][self.config.center.key] or self.debuff
-                print(mod.config["Disabled Things"][self.config.center.key])
+                mod.config["Disabled Things"][antiVal] = not mod.config["Disabled Things"][antiVal] or self.debuff
+                --print(mod.config["Disabled Things"][antiVal])
                 SMODS.save_mod_config(mod)
             end
             end
         end
     end
+    return ret
+end
+
+local oldfunc = get_new_boss
+function get_new_boss()
+    
+    local ret = oldfunc()
+    if mod.config['Disabled Things'][ret] then
+        local num = 0
+        for k,v in pairs(G.P_BLINDS) do
+            if mod.config['Disabled Things'][k] then
+                num = num+1
+            end
+        end
+        if num == #G.P_BLINDS - 2 then
+            return 'bl_big'
+        end
+        return get_new_boss()
+    end
+    
     return ret
 end
 
@@ -89,5 +128,49 @@ function get_current_pool(_type, _rarity, _legendary, _append)
     end
     return truepool,_poolkey
 end
+
+
+
+
+G.FUNCS.enable_all = function(e)
+    mod.config["Disabled Things"]  = {
+
+    }
+end
+
+
+local jimboTabs = function() return {
+	{
+		label = "Config",
+		chosen = true,
+		tab_definition_function = function()
+			jimbo_nodes = {}
+			settings = { n = G.UIT.C, config = { align = "tm", padding = 0.05 }, nodes = {} }
+
+            settings.nodes[#settings.nodes + 1] =
+                UIBox_button({button = 'enable_all', label = {"Reset Disabled"},  minw = 5, minh = 1.7, scale = 0.6, id = 'enable_all'})
+			config = { n = G.UIT.R, config = { align = "tm", padding = 0 }, nodes = { settings } }
+			jimbo_nodes[#jimbo_nodes + 1] = config
+			return {
+				n = G.UIT.ROOT,
+				config = {
+					emboss = 0.05,
+					minh = 6,
+					r = 0.1,
+					minw = 10,
+					align = "cm",
+					padding = 0.2,
+					colour = G.C.BLACK,
+				},
+				nodes = jimbo_nodes,
+			}
+		end,
+	},
+} 
+end
+SMODS.current_mod.extra_tabs = jimboTabs
+
+
+
 ----------------------------------------------
 ------------MOD CODE END----------------------
